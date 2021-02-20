5fc6062cab5a1734e8b756a3bc100f4f430c703c
Adrian Ciołek <qwizi95@protonmail.com>
2021-02-20 18:01:52 +0100

    Created permission service

--- src/permissions/permissions.module.ts
+++ src/permissions/permissions.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import {TypeOrmModule} from "@nestjs/typeorm";
 import {Permission} from "./permission.entity";
+import { PermissionsService } from './permissions.service';
 
 @Module({
-    imports: [TypeOrmModule.forFeature([Permission])]
+    imports: [TypeOrmModule.forFeature([Permission])],
+    providers: [PermissionsService],
+    exports: [PermissionsService]
 })
 export class PermissionsModule {}
