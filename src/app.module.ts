446a5aefa345d5658abf0f20d92ee164c9c29704
Adrian Ciołek <qwizi95@protonmail.com>
2021-02-20 17:39:55 +0100

    SHAR-7 configured database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import {TypeOrmModule} from "@nestjs/typeorm";
+import {ConfigModule} from "@nestjs/config";
 
 @Module({
-  imports: [],
+  imports: [
+      ConfigModule.forRoot({
+        isGlobal: true
+      }),
+      TypeOrmModule.forRoot({
+        type: 'mysql',
+        host: process.env.DB_HOST,
+        port: 3306,
+        username: process.env.DB_USER,
+        password: process.env.DB_PASS,
+        database: process.env.DB_DATABASE,
+        entities: [],
+        synchronize: true,
+      })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
