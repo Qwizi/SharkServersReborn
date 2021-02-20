48ec28267174a8a4be5fc2ed7d9320fdb1b9b01b
Adrian Ciołek <qwizi95@protonmail.com>
2021-02-20 17:52:03 +0100

    SHAR-56 created permissions.enum.ts file

---
+++ src/permissions/permissions.enum.ts
@@ -0,0 +1,11 @@
+export enum Perms {
+    SERVICE_CREATE = "service.create",
+    SERVICE_FIND_ALL = "service.find_all",
+    SERVICE_FIND = "service.find",
+    SERVICE_UPDATE = 'service.update',
+    SERVICE_DELETE = 'service.delete',
+    CONTROLLER_CREATE = 'controller.create',
+    CONTROLLER_FIND_ALL = 'controller.find_all',
+    CONTROLLER_UPDATE = 'controller.update',
+    CONTROLLER_DELETE = 'controller.delete'
+}
