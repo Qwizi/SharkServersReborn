28869bc75f977a0519b38bd384e973011e9189e6
Adrian Ciołek <qwizi95@protonmail.com>
2021-02-21 17:36:31 +0100

    SHAR-57 SHAR-44 created service find and findone method

--- src/roles/roles.service.ts
+++ src/roles/roles.service.ts
@@ -4,6 +4,9 @@ import {Role} from "./roles.entity";
 import {Repository} from "typeorm";
 import {CreateRoleDto} from "./dto/createRole.dto";
 import {PermissionsService} from "../permissions/permissions.service";
+import {FindManyOptions} from "typeorm/find-options/FindManyOptions";
+import {FindOneOptions} from "typeorm/find-options/FindOneOptions";
+import {Permission} from "../permissions/permissions.entity";
 
 @Injectable()
 export class RolesService implements OnModuleInit {
@@ -13,12 +16,21 @@ export class RolesService implements OnModuleInit {
     ) {}
 
     async onModuleInit() {
-        
+        const role = await this.findOne({where: {id: 1}, relations: ['permissions']});
+        console.log(role);
     }
 
     async create(createRoleDto: CreateRoleDto): Promise<Role | undefined> {
         const newRole = this.rolesRepository.create(createRoleDto);
         await this.rolesRepository.save(newRole);
         return newRole;
+    }
+
+    async find(options?: FindManyOptions): Promise<Role[] | []> {
+        return this.rolesRepository.find(options);
+    }
+
+    async findOne(options?: FindOneOptions<Role>): Promise<Role | undefined> {
+        return this.rolesRepository.findOne(options);
     }
 }
