dedc10fb8e5fcc71f8be43e6950d60e2b2fee49f
Adrian Ciołek <qwizi95@protonmail.com>
2021-02-21 16:44:38 +0100

    SHAR-28 created role entity

---
+++ src/roles/roles.entity.ts
@@ -0,0 +1,35 @@
+import {
+    Column,
+    CreateDateColumn,
+    Entity,
+    JoinTable,
+    ManyToMany,
+    PrimaryGeneratedColumn,
+    UpdateDateColumn
+} from "typeorm";
+import {Permission} from "../permissions/permissions.entity";
+
+@Entity()
+export class Role {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    name: string;
+
+    @Column()
+    display_name: string;
+
+    @Column({default: 'gray'})
+    color: string;
+
+    @ManyToMany(() => Permission)
+    @JoinTable()
+    permissions: Permission[];
+
+    @CreateDateColumn()
+    public created_at: Date;
+
+    @UpdateDateColumn()
+    public updated_at: Date;
+}
