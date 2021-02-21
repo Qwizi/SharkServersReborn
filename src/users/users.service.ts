a86b88f7a62a4f460b2d7d15739587b7c280c370
Adrian Ciołek <qwizi95@protonmail.com>
2021-02-21 19:12:56 +0100

    SHAR-15 created service create method

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,24 @@
-import { Injectable } from '@nestjs/common';
+import {Injectable, Logger, OnModuleInit} from '@nestjs/common';
+import {InjectRepository} from "@nestjs/typeorm";
+import {User} from "./users.entity";
+import {Repository} from "typeorm";
+import {CreateUserDto} from "./dto/createUser.dto";
 
 @Injectable()
-export class UsersService {}
+export class UsersService implements OnModuleInit {
+    private logger = new Logger(UsersService.name);
+    constructor(
+        @InjectRepository(User) private usersRepository: Repository<User>
+    ) {}
+
+    async onModuleInit() {
+        this.logger.log('UsersService dziala');
+    }
+
+    async create(createUserDto: CreateUserDto): Promise<User | undefined> {
+        const user = await this.usersRepository.create(createUserDto)
+        await this.usersRepository.save(user);
+        return user;
+    }
+
+}
