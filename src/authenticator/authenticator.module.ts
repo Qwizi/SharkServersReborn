315c5d814287084aa0f46ef3ba8d62130009efda
Adrian Ciołek <qwizi95@protonmail.com>
2021-02-22 17:02:13 +0100

    SHAR-77 created operations service

--- src/authenticator/authenticator.module.ts
+++ src/authenticator/authenticator.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import {TypeOrmModule} from "@nestjs/typeorm";
 import {Operation} from "./operation.entity";
+import {OperationsService} from "./operations.service";
 
 @Module({
-    imports: [TypeOrmModule.forFeature([Operation])]
+    imports: [TypeOrmModule.forFeature([Operation])],
+    providers: [OperationsService]
 })
 export class AuthenticatorModule {}
