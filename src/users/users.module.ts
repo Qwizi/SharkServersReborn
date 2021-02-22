e1e23776a0ccf3eb17a7b1d2936c469a09ceed8c
Adrian Ciołek <qwizi95@protonmail.com>
2021-02-22 18:50:47 +0100

    SHAR-76 created authenticator service createCode method

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,11 +3,13 @@ import {TypeOrmModule} from "@nestjs/typeorm";
 import {User} from "./users.entity";
 import { UsersService } from './users.service';
 import {RolesModule} from "../roles/roles.module";
+import {AuthenticatorModule} from "../authenticator/authenticator.module";
 
 @Module({
     imports: [
         TypeOrmModule.forFeature([User]),
-        RolesModule
+        RolesModule,
+        AuthenticatorModule
     ],
     providers: [UsersService],
     exports: [UsersService]
