49147983c909a390e16c455c0b1d56ce49ab2c50
Adrian Ciołek <qwizi95@protonmail.com>
2021-02-22 17:16:33 +0100

    SHAR-78 created operation service create method

---
+++ src/authenticator/dto/createOperation.dto.ts
@@ -0,0 +1,15 @@
+import {IsEnum, IsString, Length, ValidateNested} from "class-validator";
+import {Operations} from "../operations.enums";
+import {User} from "../../users/users.entity";
+
+export class CreateOperationDto {
+    @Length(6, 6)
+    @IsString()
+    code: string;
+
+    @IsEnum(Operations)
+    type: Operations
+
+    @ValidateNested()
+    user: User
+}
