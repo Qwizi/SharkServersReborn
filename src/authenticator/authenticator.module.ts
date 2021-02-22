30effdd7d89ca7061febb7c11c873c347edce7ed
Adrian Ciołek <qwizi95@protonmail.com>
2021-02-22 17:35:35 +0100

    SHAR-72 installed and configured cache module

--- src/authenticator/authenticator.module.ts
+++ src/authenticator/authenticator.module.ts
@@ -1,10 +1,17 @@
-import { Module } from '@nestjs/common';
+import {CacheModule, Module} from '@nestjs/common';
 import {TypeOrmModule} from "@nestjs/typeorm";
 import {Operation} from "./operation.entity";
 import {OperationsService} from "./operations.service";
-
+import * as redisStore from 'cache-manager-redis-store';
 @Module({
-    imports: [TypeOrmModule.forFeature([Operation])],
+    imports: [
+        TypeOrmModule.forFeature([Operation]),
+        CacheModule.register({
+            store: redisStore,
+            host: process.env.REDIS_HOST,
+            port: process.env.REDIS_PORT
+        })
+    ],
     providers: [OperationsService],
     exports: [OperationsService]
 })
