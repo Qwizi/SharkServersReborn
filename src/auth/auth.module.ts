c9779128fdc939053d2463d13c8d006d0374df62
Adrian Ciołek <qwizi95@protonmail.com>
2021-02-23 22:13:59 +0100

    SHAR-91, SHAR-92, SHAR-93

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,4 +1,13 @@
 import { Module } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import {PassportModule} from "@nestjs/passport";
+import {LocalStrategy} from "./strategies/local.strategy";
+import {UsersModule} from "../users/users.module";
+import { AuthController } from './auth.controller';
 
-@Module({})
+@Module({
+  imports: [UsersModule, PassportModule.register({session: true})],
+  providers: [AuthService, LocalStrategy],
+  controllers: [AuthController]
+})
 export class AuthModule {}
