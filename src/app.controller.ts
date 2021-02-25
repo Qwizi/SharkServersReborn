77c3ce380447e74701e1100a2c752b2f12184e6e
Adrian Ciołek <qwizi95@protonmail.com>
2021-02-25 19:18:59 +0100

    SHAR-95 created express auth session

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,6 @@
-import {Controller, Get, Post, Request, Res, UseGuards} from '@nestjs/common';
+import {Controller, Get, Post, Req, Request, Res, UseGuards} from '@nestjs/common';
 import { AppService } from './app.service';
+import {AuthenticatedGuard} from "./auth/guards/authenticated.guard";
 
 @Controller()
 export class AppController {
@@ -8,5 +9,11 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @UseGuards(AuthenticatedGuard)
+  @Get('profile')
+  async profile(@Req() req)  {
+    return req.user;
   }
 }
--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,12 +1,19 @@
-import {Controller, Post, Request, UseGuards} from '@nestjs/common';
+import {Controller, Get, Post, Req, Request, Res, UseGuards} from '@nestjs/common';
 import {LocalAuthGuard} from "./guards/local-auth.guard";
+import {LoginGuard} from "./guards/login.guard";
 
 @Controller('auth')
 export class AuthController {
-    @UseGuards(LocalAuthGuard)
-    @Post('auth/login')
+    @UseGuards(LoginGuard)
+    @Post('login')
     async login(@Request() req) {
         console.log(req.user);
         return req.user;
+    }
+
+    @Get("logout")
+    logout(@Req() req, @Res() res) {
+        req.logout();
+        res.redirect('/')
     }
 }
