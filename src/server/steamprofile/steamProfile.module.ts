d440e1ec0d5206b17082e672dcca8e2cc2d90dcb
Adrian Ciołek <qwizi95@protonmail.com>
2021-03-06 17:26:28 +0100

    SHAR-155 created create steamprofile method

---
+++ src/server/steamprofile/steamProfile.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import {TypeOrmModule} from "@nestjs/typeorm";
+import {SteamProfile} from "./steamProfile.entity";
+import { SteamProfileService } from './steamProfile.service';
+
+@Module({
+    imports: [TypeOrmModule.forFeature([SteamProfile])],
+    providers: [SteamProfileService],
+    exports: [SteamProfileService]
+})
+export class SteamProfileModule {}
--- src/server/steamprofile/steamprofile.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import {TypeOrmModule} from "@nestjs/typeorm";
-import {SteamProfile} from "./steamprofile.entity";
-
-@Module({
-    imports: [TypeOrmModule.forFeature([SteamProfile])],
-})
-export class SteamprofileModule {}
