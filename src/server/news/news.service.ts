6658776104eb50921e1a0a8468c7bbd1b61f192a
Adrian Ciołek <qwizi95@protonmail.com>
2021-03-08 10:30:40 +0100

    SHAR-173, SHAR-174

--- src/server/news/news.service.ts
+++ src/server/news/news.service.ts
@@ -1,4 +1,24 @@
-import { Injectable } from '@nestjs/common';
+import {Injectable, OnModuleInit} from '@nestjs/common';
+import {InjectRepository} from "@nestjs/typeorm";
+import {News} from "./news.entity";
+import {Repository} from "typeorm";
+import {FindManyOptions} from "typeorm/find-options/FindManyOptions";
+import {FindOneOptions} from "typeorm/find-options/FindOneOptions";
 
 @Injectable()
-export class NewsService {}
+export class NewsService implements OnModuleInit {
+    constructor(@InjectRepository(News) private newsService: Repository<News>) {}
+
+    async onModuleInit() {
+        console.log(await this.find());
+        console.log(await this.findOne());
+    }
+
+    async find(options?: FindManyOptions): Promise<News[] | []> {
+        return this.newsService.find(options);
+    }
+
+    async findOne(options?: FindOneOptions<News>): Promise<News | undefined> {
+        return this.newsService.findOne(options);
+    }
+}
