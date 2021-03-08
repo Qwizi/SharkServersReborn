b0f8d8f4cdc0502c72cd440fd49d839454f3b000
Adrian Ciołek <qwizi95@protonmail.com>
2021-03-08 10:52:47 +0100

    SHAR-172, SHAR-171

---
+++ src/server/news/dto/updateNews.dto.ts
@@ -0,0 +1,17 @@
+import {IsOptional, IsString, Length, ValidateNested} from "class-validator";
+import {User} from "../../users/users.entity";
+
+export class UpdateNewsDto {
+    @IsString()
+    @IsOptional()
+    @Length(6, 80)
+    title?: string;
+
+    @IsString()
+    @IsOptional()
+    content?: string;
+
+    @ValidateNested()
+    @IsOptional()
+    author?: User
+}
--- src/server/news/news.service.ts
+++ src/server/news/news.service.ts
@@ -8,6 +8,8 @@ import {CreateNewsDto} from "./dto/createNews.dto";
 import slugify from "slugify";
 import {User} from "../users/users.entity";
 import {UsersService} from "../users/users.service";
+import {RemoveOptions} from "typeorm/browser";
+import {UpdateNewsDto} from "./dto/updateNews.dto";
 
 @Injectable()
 export class NewsService implements OnModuleInit {
@@ -40,5 +42,17 @@ export class NewsService implements OnModuleInit {
 
     async findOne(options?: FindOneOptions<News>): Promise<News | undefined> {
         return this.newsRepository.findOne(options);
+    }
+
+    async update(news: News, updateNewsDto: UpdateNewsDto): Promise<News> {
+        news.title = updateNewsDto.title || news.title;
+        news.content = updateNewsDto.content || news.content;
+        news.author = updateNewsDto.author || news.author;
+        await this.newsRepository.save(news);
+        return news;
+    }
+
+    async remove(entity: News, options?: RemoveOptions): Promise<any> {
+        return options ? this.newsRepository.remove(entity, options) : this.newsRepository.remove(entity);
     }
 }
