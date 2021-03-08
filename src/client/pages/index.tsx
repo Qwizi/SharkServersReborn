24fb2fddc812d6b790fcc9905c6ea20e89c7f0c8
Adrian Ciołek <qwizi95@protonmail.com>
2021-03-08 10:11:53 +0100

    Updated layout

--- src/client/pages/index.tsx
+++ src/client/pages/index.tsx
@@ -1,6 +1,6 @@
 import React, {useEffect} from 'react'
 import {withAuthServerSideProps} from "../hocs/withAuth";
-import {Badge, Card, Col, ProgressBar, Row} from "react-bootstrap";
+import {Badge, Card, Col, ListGroup, ProgressBar, Row} from "react-bootstrap";
 import {Servers} from "../components/index/servers.component";
 import {ShopServices} from "../components/index/shopServices";
 import {News} from "../components/index/news.component";
@@ -12,9 +12,77 @@ const Index = ({user}: {user:any}) => {
             <Col lg={4}>
                <Servers />
                <ShopServices/>
+                <Row>
+                    <Col>
+                        <Row>
+                            <Col>
+                                <h4>Ostatnie podania</h4>
+                            </Col>
+                        </Row>
+                        <Row>
+                            <Col>
+                                <Card>
+                                    <Card.Body>
+                                        <ListGroup>
+                                            <ListGroup.Item>Link</ListGroup.Item>
+                                        </ListGroup>
+                                    </Card.Body>
+                                </Card>
+                            </Col>
+                        </Row>
+                    </Col>
+                </Row>
             </Col>
             <Col lg={{offset: 1 }}>
                 <News />
+                <br/>
+                <Row>
+                    <Col>
+                        <Row>
+                            <Col>
+                                <h4>Rekrutacja</h4>
+                            </Col>
+                        </Row>
+                        <Row>
+                            <Col lg={6}>
+                                <Card>
+                                    <Card.Body>
+                                        <Card.Title>
+                                            Admin Jailbreak <Badge variant={"success"}>2/4</Badge>
+                                        </Card.Title>
+                                    </Card.Body>
+                                </Card>
+                            </Col>
+                            <Col lg={6}>
+                                <Card>
+                                    <Card.Body>
+                                        <Card.Title>
+                                            Admin Jailbreak <Badge variant={"success"}>2/4</Badge>
+                                        </Card.Title>
+                                    </Card.Body>
+                                </Card>
+                            </Col>
+                            <Col lg={6}>
+                                <Card>
+                                    <Card.Body>
+                                        <Card.Title>
+                                            Admin Jailbreak <Badge variant={"success"}>2/4</Badge>
+                                        </Card.Title>
+                                    </Card.Body>
+                                </Card>
+                            </Col>
+                            <Col lg={6}>
+                                <Card>
+                                    <Card.Body>
+                                        <Card.Title>
+                                            Opiekun Jailbreak <Badge variant={"secondary"}>1/1</Badge>
+                                        </Card.Title>
+                                    </Card.Body>
+                                </Card>
+                            </Col>
+                        </Row>
+                    </Col>
+                </Row>
             </Col>
         </Row>
     )
