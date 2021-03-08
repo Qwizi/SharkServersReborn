88f447911043204a95eafec5d0e1f07d004878bd
Adrian Ciołek <qwizi95@protonmail.com>
2021-03-08 10:27:39 +0100

    SHAR-167

--- src/server/news/news.module.ts
+++ src/server/news/news.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
 import { NewsService } from './news.service';
+import {TypeOrmModule} from "@nestjs/typeorm";
+import {News} from "./news.entity";
 
 @Module({
+  imports: [TypeOrmModule.forFeature([News])],
   providers: [NewsService]
 })
 export class NewsModule {}
