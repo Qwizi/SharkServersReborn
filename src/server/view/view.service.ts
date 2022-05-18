24877e36e9b9cf2274913872427e275319ea9789
Adrian Ciołek <ciolek.adrian@protonmail.com>
2022-05-18 08:56:43 +0200

    Small changes

--- src/server/view/view.service.ts
+++ src/server/view/view.service.ts
@@ -1,6 +1,7 @@
 import {Injectable, Logger, OnModuleInit} from '@nestjs/common';
 import next from 'next';
-import NextServer from 'next/dist/next-server/server/next-server';
+import { NextServer } from 'next/dist/server/next';
+//import NextServer from 'next/dist/next-server/server/next-server';
 @Injectable()
 export class ViewService implements OnModuleInit {
     private logger = new Logger(ViewService.name);
