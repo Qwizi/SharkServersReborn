6352a2f35ce435942beb1b76a4baf50b7a4abb7b
Adrian Ciołek <ciolek.adrian@protonmail.com>
2022-05-24 17:05:53 +0200

    Added news and permissions controllers

--- src/server/news/news.module.ts
+++ src/server/news/news.module.ts
@@ -4,13 +4,16 @@ import {TypeOrmModule} from "@nestjs/typeorm";
 import {News} from "./news.entity";
 import {UsersModule} from "../users/users.module";
 import { NewsController } from './controllers/news.controller';
+import { NewsV2Controller } from './controllers/newsv2.controller';
+import { NewsV2Service } from './services/newsv2.service';
 
 @Module({
   imports: [
       TypeOrmModule.forFeature([News]),
 
   ],
-  providers: [NewsService],
-  controllers: [NewsController]
+  providers: [NewsService, NewsV2Service],
+  exports: [NewsV2Service],
+  controllers: [NewsController, NewsV2Controller]
 })
 export class NewsModule {}
---
+++ src/server/permissions/controllers/permissions.controller.ts
@@ -0,0 +1,24 @@
+import { Controller } from "@nestjs/common";
+import { ApiTags } from "@nestjs/swagger";
+import { Crud, CrudController } from "@nestjsx/crud";
+import {Permission } from "../permissions.entity"
+import { PermissionsV2Service } from "../services/permissionsv2.service";
+
+@ApiTags("permissions")
+@Crud({
+    model: {
+        type: Permission,
+    },
+    routes: {
+        only: ["getOneBase", "getManyBase",]
+    }
+})
+@Controller({
+    version: "2",
+    path: "permissions"
+})
+export class PermissionsController implements CrudController<Permission> {
+    constructor(
+        public service: PermissionsV2Service
+    ) {}
+}
