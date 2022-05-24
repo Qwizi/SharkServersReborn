6f46d20b2b3720ff84fec56f5948371eeafad6f4
Adrian Ciołek <ciolek.adrian@protonmail.com>
2022-05-24 17:05:53 +0200

    Added news and permissions controllers

---
+++ src/server/news/controllers/newsv2.controller.ts
@@ -0,0 +1,29 @@
+import { Controller } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
+import {Crud, CrudController} from "@nestjsx/crud";
+import {News} from "../news.entity";
+import { NewsV2Service } from '../services/newsv2.service';
+
+@ApiTags('news')
+@Crud({
+    model: {
+        type: News
+    },
+    routes: {
+        only: ["getOneBase", "getManyBase"]
+    },
+    params: {
+        slug: {
+            field: 'slug',
+            type: "string",
+            primary: true
+        },
+    }
+})
+@Controller({
+    version: '2',
+    path: "news"
+})
+export class NewsV2Controller implements CrudController<News> {
+    constructor(public service: NewsV2Service) {}
+}
--- src/server/permissions/permissions.module.ts
+++ src/server/permissions/permissions.module.ts
@@ -4,6 +4,8 @@ import {Permission} from "./permissions.entity";
 import { PermissionsService } from './services/permissions.service';
 import {PermissionModuleOptions} from "./permissions.types";
 import {PERMISSIONS_OPTIONS} from "./permissions.constansts";
+import { PermissionsController } from './controllers/permissions.controller';
+import { PermissionsV2Service } from './services/permissionsv2.service';
 
 @Global()
 @Module({})
@@ -17,9 +19,11 @@ export class PermissionsModule {
                     provide: PERMISSIONS_OPTIONS,
                     useValue: options
                 },
-                PermissionsService
+                PermissionsService,
+                PermissionsV2Service,
             ],
-            exports: [PermissionsService]
+            controllers: [PermissionsController],
+            exports: [PermissionsService, PermissionsV2Service],
         }
     }
 }
